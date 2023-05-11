1705215c4b16e71913a4503a5e19636902814d2d
AjitUpadhyay-0 <130857793+AjitUpadhyay-0@users.noreply.github.com>
2023-05-11 09:18:43 +0530

    Add files via upload

---
+++ Python/Activities/Activity15.py
@@ -0,0 +1,9 @@
+"""Handling Errors
+Activity 15
+Write a Python program that throws a NameError.
+Handle the error so it doesn't halt execution."""
+try:
+    #x=10
+    print(x)
+except NameError:
+    print("x hasn't been defined yet.")
